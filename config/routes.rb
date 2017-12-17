984ec3924e6152f3bf04e55fae0b79e79f9ed5ff
Artur Malabarba <artur@endlessparentheses.com>
2017-12-16 22:02:48 -0200

    ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   resources :sale_listings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
