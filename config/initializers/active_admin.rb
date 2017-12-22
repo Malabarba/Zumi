9e58ef5167fd300a186a57b63390a7f335a27657
Artur Malabarba <artur@endlessparentheses.com>
2017-12-21 22:03:12 -0200

    No activeadmin footer

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -284,7 +284,7 @@ ActiveAdmin.setup do |config|
   # By default, the footer shows the current Active Admin version. You can
   # override the content of the footer here.
   #
-  # config.footer = 'my custom footer text'
+  config.footer = nil
 
   # == Sorting
   #
