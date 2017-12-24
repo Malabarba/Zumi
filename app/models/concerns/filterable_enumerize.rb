4cdf7ba0fb22b67c3df58a2dab739a0e5d1a2e95
Artur Malabarba <artur@endlessparentheses.com>
2017-12-24 08:54:17 -0200

    Improve enumerize

---
+++ app/models/concerns/filterable_enumerize.rb
@@ -0,0 +1,19 @@
+module FilterableEnumerize
+  extend ActiveSupport::Concern
+
+  included do
+    extend Enumerize
+
+    def self.enumerize(column, **_)
+      define_singleton_method :"#{column}_filter_opts" do
+        klass = self
+        { as: :check_boxes,
+          label: -> { klass.human_attribute_name(column) },
+          collection: -> { klass.send(column).values.map { |value| [value.text, value] } } }
+      end
+      super
+      hash = {activerecord: {attributes: {model_name.param_key => {"#{column}_text" => human_attribute_name(column)}}}}
+      I18n.backend.store_translations(:'pt-BR', hash)
+    end
+  end
+end
